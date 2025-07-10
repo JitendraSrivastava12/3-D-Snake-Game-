15693fb704657f7fc4734ca22c0f4f51a80e503d
Jitendra Srivastav <161929034+JitendraSrivastava12@users.noreply.github.com>
2025-07-11 00:21:31 +0530

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -84,7 +84,8 @@ class SnakeGame:
 # ---------- Video Processor ----------
 class GameProcessor(VideoProcessorBase):
     def __init__(self):
-        self.detector = HandDetector(detectionCon=0.5, maxHands=1)
+        # ✅ modelComplexity=0 disables GPU/OpenGL (fixes EGL errors)
+        self.detector = HandDetector(detectionCon=0.5, maxHands=1, modelComplexity=0)
         self.game = SnakeGame()
         self.last_time = time.time()
 
